374ab31806fb45c66438e1456e8ca9085e1f7483
Palash chandra <dlab20@droitlab.com>
2020-10-29 20:01:01 +0600

    29 oct

--- js/slider.js
+++ js/slider.js
@@ -59,3 +59,108 @@ var pc_element = pc_element || {};
         }
     }, p(document).ready(pc_element.documentOnReady.init)
 }(jQuery);
+
+
+
+
+
+
+
+//swiper slider multi thumb slider option
+const dlmultiSwiperSlides = function () {
+    let sliderMain = document.querySelectorAll('.swiper-container.dl_thumb_slider_main')
+    let sliderNav = document.querySelectorAll('.swiper-container.dl_thumb_slider_nav')
+    let mainArray = [];
+    let navArray = [];
+    
+    sliderMain.forEach(function (element, i) {
+        var mself = sliderMain;
+        var per = ($(mself[i]).attr("id"), $(mself[i]).data("perpage") || 1);
+        var lop = $(mself[i]).data("loop");
+        var spd = $(mself[i]).data("speed") || 1000;
+        var sps = $(mself[i]).data("space") || 0;
+        var afc = $(mself[i]).data("effect");
+        var atp = $(mself[i]).data("main_autoplay");
+        var msl = $(mself[i]).data("mainslideloop") || 7;
+        var maincents = $(mself[i]).data("main_centere");
+        var main_br = $(mself[i]).data("breakpoints");
+        var main_nx = $(mself[i]).data("main_swiper_next");
+        var main_pv = $(mself[i]).data("main_swiper_prev");
+        var dir = $(mself[i]).data("direction") || "horizontal";
+        mainArray.push(
+            new Swiper(element, {
+                loop: lop,
+                loopedSlides: msl,
+                slidesPerGroup: 1,
+                slidesPerView: per,
+                loopFillGroupWithBlank: true,
+                speed: spd,
+                effect: afc,
+                spaceBetween: sps,
+                autoplay: atp,
+                centeredSlides: maincents,
+                breakpoints: main_br,
+                direction: dir,
+                // navigation: {
+                //     nextEl: mself[i].querySelector(main_nx),
+                //     prevEl: mself[i].querySelector(main_pv)
+                // }
+                navigation: {
+                    nextEl: main_nx,
+                    prevEl: main_pv
+                }
+            })
+        );
+    });
+    sliderNav.forEach(function (element, i) {
+        var self = sliderNav;
+        var navper = ($(self[i]).attr("id"), $(self[i]).data("nav_perpage") || 1);
+        var navlop = $(self[i]).data("nav_loop");
+        var navspd = $(self[i]).data("nav_speed") || 1000;
+        var navsps = $(self[i]).data("nav_space") || 0;
+        var navafc = $(self[i]).data("nav_effect");
+        var navatp = $(self[i]).data("nav_autoplay");
+        var navcents = $(self[i]).data("nav_centere");
+        var nsl = $(self[i]).data("navslideloop") || 7;
+        var nav_br = $(self[i]).data("breakpoints");
+        var nav_nx = $(self[i]).data("navswipernext");
+        var nav_pv = $(self[i]).data("navswiperprev");
+        var navdir = $(self[i]).data("nav_direction") || "horizontal";
+        navArray.push(
+            new Swiper(element, {
+                slidesPerView: navper,
+                loop: navlop,
+                loopedSlides: nsl,
+                slidesPerGroup: 1,
+                slideToClickedSlide: true,
+                spaceBetween: navsps,
+                loopFillGroupWithBlank: true,
+                centeredSlides: navcents,
+                direction: navdir,
+                autoplay: navatp,
+                speed: navspd,
+                effect: navafc,
+                breakpoints: nav_br,
+                // navigation: {
+                //     nextEl: self[i].querySelector(nav_nx),
+                //     prevEl: self[i].querySelector(nav_pv)
+                // }
+                navigation: {
+                    nextEl: nav_nx,
+                    prevEl: nav_pv
+                }
+            })
+        );
+    });
+    // const checkOnPage = function () {
+    //     if (sliderMain.length > 0 && sliderNav.length > 0) {
+    //         let numberOfSlides = mainArray.length || navArray.length || 0;
+    //         for (let i = 0; i < numberOfSlides; i++) {
+    //             mainArray[i].controller.control = navArray[i];
+    //             navArray[i].controller.control = mainArray[i];
+    //         }
+    //     }
+    // }
+    // checkOnPage();
+}
+dlmultiSwiperSlides();
